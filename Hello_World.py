0a9ae8deceb68bb25260060f4eb25dd9bc5b5286
Chlorine Trifluoride <s2510423@siheung.hs.kr>
2025-11-20 10:06:49 +0900

    fafdafaf

--- Hello_World.py
+++ Hello_World.py
@@ -8,9 +8,10 @@ numlist = [ 1, 6, 3, 3, 2, 11, 9, 2, 8, 3, 10, 11, 4, 2, 7, 11, 5, 8, 6, 11, 0, 
 
 pr = ''
 for i in numlist:
-    for j in range(10):
+    for j in range(100):
         letter = random.choice(string.ascii_letters)
         print( pr + letter, end='\r')
+        time.sleep(0.05)
     pr +=strlist[i]
 
         
